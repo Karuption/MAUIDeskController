2c55bfb99a96592666b249329448d273b3c3eff5
Karuption <pederson26@gmail.com>
2022-11-19 19:27:39 -0600

    Add project files.

---
+++ MAUIDeskController/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIDeskController;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUIDeskController/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MAUIDeskController;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
